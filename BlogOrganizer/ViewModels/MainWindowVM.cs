02b11749780be84277cc395c1028ca7ce13ae63a
zeikomi552 <68983834+zeikomi552@users.noreply.github.com>
2021-06-06 18:16:24 +0900

    ファーストコミット
    Signed-off-by: zeikomi552 <68983834+zeikomi552@users.noreply.github.com>

---
+++ BlogOrganizer/ViewModels/MainWindowVM.cs
@@ -0,0 +1,22 @@
+﻿using MVVMCore.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogOrganizer.ViewModels
+{
+    public class MainWindowVM : ViewModelBase
+    {
+        public override void Init(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void Close(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();   
+        }
+    }
+}
